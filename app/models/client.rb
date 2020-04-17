8a2d0babf8fb8c0f4e157f1f32cc57a4b0c6d7c6
nicmlu <NicoleMLucien@gmail.com>
2020-04-17 15:32:55 -0400

    add client associations and validations

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,11 @@
 class Client < ApplicationRecord
+    has_secure_password 
+
+    has_many :appointments 
+    has_many :reviews 
+    has_many :services, through: :appointments
+
+    validates_presence_of :first_name, :last_name
+    validates_format_of :phone, :with => /[0-9]{3}-[0-9]{3}-[0-9]{4}/, :message => “Phone numbers must be in xxx-xxx-xxxx format.”
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
