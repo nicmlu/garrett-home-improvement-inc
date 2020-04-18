93f363bc5b7642e4fd9d5537c09e1b5932f6f5d8
nicmlu <NicoleMLucien@gmail.com>
2020-04-17 23:58:58 -0400

    add application helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def current_client 
+    @current_client ||= Client.find_by_id(session[:client_id]) if session[:client_id]
+  end 
+
+  def logged_in?
+    !!session[:client_id]
+  end 
+
+  def redirect_if_not_logged_in
+    redirect_to '/' if !logged_in?
+  end 
 end
