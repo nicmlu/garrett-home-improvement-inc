842e1d3e388be5a269dae9869048702a00b6ec52
nicmlu <NicoleMLucien@gmail.com>
2020-04-18 00:55:25 -0400

    fix before pictures active storage attributes and appointment show page code to include before pictures

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,5 @@
 class AppointmentsController < ApplicationController
+    before_action :login_required
 
     def new
         @appointment = Appointment.new(client_id: params[:client_id]) 
@@ -9,10 +10,21 @@ class AppointmentsController < ApplicationController
     end 
 
     def create
+        @appointment = current_user.appointments.build(appointment_params)
+        if @appointment.save
+           redirect_to appointment_path(@appointment)
+        else 
+          flash[:message] = "Appointment was not booked! Please try again."
+          render :new 
+        end
     end 
 
     private
 
-    
+    def appointment_params
+        params.require(:appointment).permit(:date, :time, :budget, :notes, :appointment_image, :client_id, :service_id)
+    end 
+
+
 
 end 
